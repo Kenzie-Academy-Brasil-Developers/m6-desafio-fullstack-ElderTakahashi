a2c975e6fbf4e3d5cd21344d76fb9af7a457f8cf
Elder Toshiro Takahashi <eldertaka@gmail.com>
2024-02-23 21:07:59 -0300

    feat: deploy config for render

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,9 +5,10 @@ import swaggerDocument from "./swagger.json";
 import express, { Application, json } from "express";
 import { routes } from "./routers";
 import { handleErrors } from "./middlewares/handleErrors.middleware";
+import cors from "cors";
 
 export const app: Application = express();
-
+app.use(cors());
 app.use(json());
 
 app.use(
