e6428fe5d4f6d2bcf23da0d310fef546c5338132
Elder Toshiro Takahashi <eldertaka@gmail.com>
2024-02-26 01:38:09 -0300

    feat: readme.md

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,8 +4,3 @@ export const contactListApi = axios.create({
   baseURL: "http://localhost:3000",
   timeout: 8 * 1000,
 });
-
-// export const contactListApi = axios.create({
-//   baseURL: "https://m6-desafio-fullstack-eldertakahashi.onrender.com",
-//   timeout: 8 * 1000,
-// });
